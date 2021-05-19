51dd3d7bf1c5e30387afa5eb62cb21b94ab65137
IzumiSakai1995 <40541052+IzumiSakai1995@users.noreply.github.com>
2021-05-19 19:38:33 +0800

    webhook测试#4

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -8,7 +8,7 @@ import router from '../router/index'
 
 export default {
     name: 'Index',
-    // github webhook 测试#3
+    // github webhook 测试#4
     components: {},
     setup(){
         const goLogin = ()=>{
